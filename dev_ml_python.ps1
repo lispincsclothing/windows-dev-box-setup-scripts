f8c46d21a5c7732430e8d7ab2ed10de37fc61c61
Name <lispincsclothing@users.noreply.github.com>
2018-05-11 13:05:01 -0700

    Update dev_ml_python.ps1

--- dev_ml_python.ps1
+++ dev_ml_python.ps1
@@ -238,9 +238,10 @@ Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization
 # Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization -Name NoLockScreen -Type DWord -Value 1
 
 #Disable sleep on lid close both battery (dc) and direct (ac) power
+# DOES NOT WORK
 #https://www.tenforums.com/tutorials/69762-change-lid-close-default-action-windows-10-a.html
-powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
-powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+#powercfg -setdcvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+#powercfg -setacvalueindex SCHEME_CURRENT 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
 
 # Lock screen (not sleep) on lid close
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Power' -Name AwayModeEnabled -Type DWord -Value 1
