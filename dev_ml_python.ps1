a88c4106463262ff27403ed7be7c891e00c602b7
Name <lispincsclothing@users.noreply.github.com>
2018-05-10 23:15:50 -0700

    Update dev_ml_python.ps1

--- dev_ml_python.ps1
+++ dev_ml_python.ps1
@@ -296,7 +296,13 @@ choco install -y geforce-game-ready-driver
 #--- Map caps lock to escape ---
 $hexified = "00,00,00,00,00,00,00,00,02,00,00,00,01,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
 $kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
-New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
+IF(!(Test-Path $kbLayout)){
+	New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
+}
+ELSE {
+	New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Force -Value ([byte[]]$hexified);
+}
+
 
 #--- Rename the Computer ---
 # Requires restart, or add the -Restart flag
