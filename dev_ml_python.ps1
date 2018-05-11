bc1567885802f3f8dfa762fac7b10d5aac9fa477
Name <lispincsclothing@users.noreply.github.com>
2018-05-10 23:04:31 -0700

    Update dev_ml_python.ps1

--- dev_ml_python.ps1
+++ dev_ml_python.ps1
@@ -293,6 +293,11 @@ Install-WindowsUpdate -acceptEula
 choco install -y geforce-game-ready-driver
 #choco install -y geforce-experience
 
+#--- Map caps lock to escape ---
+$hexified = "00,00,00,00,00,00,00,00,02,00,00,00,1d,00,3a,00,00,00,00,00".Split(',') | % { "0x$_"};
+$kbLayout = 'HKLM:\System\CurrentControlSet\Control\Keyboard Layout';
+New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
+
 #--- Rename the Computer ---
 # Requires restart, or add the -Restart flag
 $computername = "RenamedPC"
